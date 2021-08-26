a5befa9dea8988231c24fb19c69a3ac7ec982d22
Andrew Tjong <72036357+andrewtjong@users.noreply.github.com>
2021-08-26 15:34:57 +0200

    set Initial Start of Drawing via Math.Random();

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # etch-a-sketch
 Vanilla JavaScript Etch-a-Sketch Project
+
+This is a personal learning project where I use the Vanilla JavaScript CanvasRenderingContext2D part of the Canvas API to draw an object on the canvas.
+
+Real World Projects that use the Canvas to draw on:
+https://hoodmaps.com/
--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -6,9 +6,22 @@ const ctx = canvas.getContext('2d');
 const shakebutton = document.querySelector('.shake');
 
 // Setup canvas for drawing
+// make a variable called height and width from the same properties on our canvas.
+const { width, height } = canvas;
+
+// create random x and y starting points on the canvas.
+
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.lineWidth = 10;
+
+ctx.beginPath(); // start the drawing
+let x = Math.floor(Math.random() * width);
+let y = Math.floor(Math.random() * height);
+
+ctx.moveTo(x, y);
+ctx.lineTo(x, y);
+ctx.stroke();
 
 // Write a draw function
 
